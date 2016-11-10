1072644d5e23788bc03f3508de1b2333b3c9db61
Sander Linke <sander@highor.com>
2016-11-10 23:53:02 +0100

    no message

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * API v1.0
+ *
+ * @author     Alexander Linke <api@highor.com>
+ * @copyright  2016 HighorBV
+ *
+ **/
+
+namespace Api;
+
+new Api;
+
+
+var_dump($_SERVER);
+var_dump($_REQUEST);
+
+class Api {
+
+
+
+}
